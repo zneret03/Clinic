8c7a61a7cd069b0bc0dcd7b64ff2f772a4e684f8
Ian <DrilonIan@yahoo.com>
2020-02-20 12:49:06 +0800

    adding appointments history

--- php-app/build/app/includes/appointments_history.php
+++ php-app/build/app/includes/appointments_history.php
@@ -2,22 +2,25 @@
 <div class="container">
     <h2>Appointments History</h2>
     <hr class="mt-5">
+    <div class="text-right">
+        <button type="button" class="btn btn-warning btn-xs"><i class="fas fa-download"></i> Export</button>
+    </div>
     <table class="table table-responsive table-striped text-center animated fadeIn" id="appointmentsTable">
     <thead>
-    <th>
-        <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-            <th>Notes</th>
-            <th>doctor</th>
-            <th>Time</th>
-            <th>Bill</th>
-            <th>Updates</th>
-            <th>Delete</th>
-        </tr>
+        <th>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Phone Number</th>
+                <th>Email</th>
+                <th>Notes</th>
+                <th>doctor</th>
+                <th>Time</th>
+                <th>Bill</th>
+                <th>Updates</th>
+                <th>Delete</th>
+            </tr>
         </th>
     </thead>
     </table>
